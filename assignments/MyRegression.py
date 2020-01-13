615bb31ec9d301bf44582dee87400dbef049fcfa
kikurin9155 <kikurin9155@gmail.com>
2020-01-13 22:14:22 +0900

    modified MyRegression

--- assignments/MyRegression.py
+++ assignments/MyRegression.py
@@ -3,15 +3,18 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
 
 class MyRegression(BaseEstimator,RegressorMixin):
-	def fit(self, X, y):
-          self.coef_ = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, y)) 
+    def fit(self, X, y):
+        X = check_array(X)
+        self.coef_ = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, y)) 
         
-          return self
+        return self
 
-        def predict(self,X):
-          y = np.dot(X, self.coef_)
-        
-          return y
+    def predict(self,X):
+        check_is_fitted(self,'coef_')
+        X = check_array(X)
+        y = np.dot(X, self.coef_)
+
+        return y
 
 if __name__=="__main__":
 	clf = MyRegression()
